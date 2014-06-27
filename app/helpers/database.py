24f585a8559d79896137fabfbe85f696d1f837d8
Michael Woods <physicsmichael@gmail.com>
2014-06-27 16:34:32 -0700

    Changed from pymysql to MySQLdb

--- app/helpers/database.py
+++ app/helpers/database.py
@@ -1,4 +1,4 @@
-import pymysql
+import MySQLdb
 import sys
 import numpy as np
 from matplotlib.path import Path
@@ -7,10 +7,11 @@ from matplotlib.path import Path
 # Returns MySQL database connection
 def con_db(host, port, user, passwd, db):
     try:
-        con = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db)
+        con = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=db)
 
-    except pymysql.Error, e:
-        print "Error %d: %s" % (e.args[0], e.args[1])
+    except:
+        print "Could not connect to database."
+        #print "Error %d: %s" % (e.args[0], e.args[1])
         sys.exit(1)
 
     return con
